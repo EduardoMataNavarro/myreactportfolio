fa33baf59a63465c2d537fdbc67ed77c4b6adea8
Eduardo I. Mata Navarro <eduardo.matanavarro1998@gmail.com>
2021-03-16 11:25:19 -0600

    16/03/2021 - First commit

---
+++ src/Components/Sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Sidebar() {
+    return (
+        <div className="container-fluid" id="main-sidebar">
+            <div className="w-100" id="sidebar-container">
+        Waddup
+            </div>
+        </div>
+    )
+}
